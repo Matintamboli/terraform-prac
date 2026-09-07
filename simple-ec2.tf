e557695f21f247ad6d427653a4da1e249cd86d1e
Matintamboli <tambolimatin313@gmail.com>
2026-09-07 13:38:46 +0530

    simple ec2 script

---
+++ simple-ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "ec2"{
+    ami = "ami-01a00762f46d584a1"
+    instance_type = "t3.micro"
+    key_name = "matin"
+
+    tags = {
+        Name = "practice"
+    }
+}
